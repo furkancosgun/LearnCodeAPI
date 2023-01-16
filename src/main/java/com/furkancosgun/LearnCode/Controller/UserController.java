2145995aa1a46b0a1ff93a3bd10a2bc97e220531
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-16 20:39:42 +0300

    Created Controller

---
+++ src/main/java/com/furkancosgun/LearnCode/Controller/UserController.java
@@ -0,0 +1,37 @@
+package com.furkancosgun.LearnCode.Controller;
+
+import com.furkancosgun.LearnCode.Model.Request.UserRequest;
+import com.furkancosgun.LearnCode.Model.User;
+import com.furkancosgun.LearnCode.Service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<?> getUserById(@PathVariable long userId){
+        return ResponseEntity.ok(userService.getUserById(userId));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> saveUser(@RequestBody User user){
+        return ResponseEntity.ok(userService.saveUser(user));
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> getUserByUsernamAndPassword(@RequestBody UserRequest user){
+        return ResponseEntity.ok(userService.getUserByUsernameAndPassword(user));
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllUsers(){
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+}
