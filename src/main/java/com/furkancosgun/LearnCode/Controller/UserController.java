2fadf10d060a883e62c20e1685fc764541ed325e
Furkan COSGUN <furkan51cosgun@gmail.com>
2023-01-16 20:46:26 +0300

    UserController

--- src/main/java/com/furkancosgun/LearnCode/Controller/UserController.java
+++ src/main/java/com/furkancosgun/LearnCode/Controller/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("/user")
+@RequestMapping("api/auth")
 public class UserController {
     @Autowired
     UserService userService;
